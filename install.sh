46f9fcad4d30d819cdbb7fe1f6674c75922a3aeb
Start_Hammond <53830640+Hammond-sys@users.noreply.github.com>
2019-08-12 05:19:12 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+#untuk menginstall bahan
+clear
+pkg install python python2
+pkg install ruby
+pkg install figlet
+pkg install toilet
+gem install lolcat
+pip2 install mechanize
+sleep 02
+clear
+echo "SALAM DARI ANJU:-)"|lolcat
+figlet HAMMOND CYBER |lolcat
+sleel 02
+clear
